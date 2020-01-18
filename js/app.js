16c46db68dc5674d6970447025750b0f8a2e1afb
Fili Santillán <fili06604@gmail.com>
2020-01-18 13:06:51 -0600

    ✨ autoplay, autopause, sw, intersectionObserver, visibilityChanged added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 import MediaPlayer from "./MediaPlayer.js";
-import AutoPlay from "./AutoPlay.js";
+import AutoPlay from "./plugins/AutoPlay.js"
+import AutoPause from "./plugins/AutoPause.js"
 
 const video = document.querySelector("video"),
       playButton = document.querySelector("#play-button"),
@@ -7,8 +8,14 @@ const video = document.querySelector("video"),
 
 const player = new MediaPlayer({ 
   el: video, 
-  plugins: [new AutoPlay()]
+  plugins: [new AutoPlay(), new AutoPause()]
 });
 
 playButton.onclick = () => player.togglePlay();
 unmuteButton.onclick = () => player.toggleMute();
+
+if("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js").catch( error => {
+    console.log(error.message);
+  })
+}
